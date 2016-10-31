6bbe9fd0c34b4bafaf0ab34ebcd4c14be5accefb
Gabriela Sampaio <gaby.sampaio@gmail.com>
2016-10-31 18:22:44 -0300

    Renaming refactoring: renaming local variable in getCharge method

--- src/Rental.groovy
+++ src/Rental.groovy
@@ -20,24 +20,24 @@ class Rental {
     }
 
     def double getCharge() {
-        double thisAmount = 0
+        double amount = 0
         //determine amounts for aRental line
         switch (this.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2
+                amount += 2
                 if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5
+                    amount += (this.getDaysRented() - 2) * 1.5
                 break
             case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3
+                amount += this.getDaysRented() * 3
                 break
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
+                amount += 1.5;
                 if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5
+                    amount += (this.getDaysRented() - 3) * 1.5
                 break
         }
-        thisAmount
+        amount
     }
 
     def getPoints() {
